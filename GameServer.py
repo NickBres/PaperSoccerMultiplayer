34c66bb96c35d78a908708fe492ac6fa0b4bf2d2
NickBres <70432147+NickBres@users.noreply.github.com>
2023-03-11 16:41:58 +0200

    working on rudp

--- GameServer.py
+++ GameServer.py
@@ -271,8 +271,6 @@ class Server:
                     sent = max(sent - window_size, 0)
                     window_size = max(window_size // 2, 1)
                     threshold = max(threshold // 2, 1)
-                    print("Packet lost")
-                    print(f"Sent {sent} packets, window size: {window_size}, threshold: {threshold}")
             except socket.timeout:
                 sent = max(sent - window_size, 0)
                 window_size = max(window_size // 2, 1)
